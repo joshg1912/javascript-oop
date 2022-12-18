a724a826202d3b67e9355be94403b4698b8bb4c1
unknown <joshgarcia7410@gmail.com>
2022-12-17 23:26:39 -0800

    adds ischeckedout method

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("test")
+class Book {
+    constructor(name, author, status){
+        this.name = name;
+        this.author = author;
+        this.status = status;
+    }
+
+    isCheckedOut(status){
+        if(status === true){
+            console.log("this book is checked out");
+        }else{
+            console.log("in stock");
+        }
+    }
+
+}
+
+let myBook = new Book('my book', 'me', false);
+
+console.log(myBook.status);
+myBook.isCheckedOut(myBook.status);
